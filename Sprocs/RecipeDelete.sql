249062298397b4891da8c6f21548cb71b6385dfe
estyg100 <estygrunwald1@gmail.com>
2024-07-30 11:51:18 -0400

    got the sproc working

--- Sprocs/RecipeDelete.sql
+++ Sprocs/RecipeDelete.sql
@@ -5,7 +5,7 @@ create or alter procedure dbo.RecipeDelete(
 as 
 begin
 	declare @return int = 0
-	if exists (select * from Recipe r where (getdate() - r.DateArchived < 30 and r.CurrentStatus = 'Archived') or r.CurrentStatus = 'Published')
+	if exists (select * from Recipe r where ((getdate() - r.DateArchived < 30 and r.CurrentStatus = 'Archived') or r.CurrentStatus = 'Published') and r.RecipeId = @RecipeId)
 	begin 
 		select @return = 1, @Message = 'Cannot delete Recipe that is currently published, or either archived for under 30 days.'
 		goto finished
